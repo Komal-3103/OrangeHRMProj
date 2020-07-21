487f5045445c57585a4c148de9dfff86711802a6
Your Name <you@example.com>
2020-07-21 21:39:22 +0530

    Uploading OrangeHrm project

---
+++ OrangeHRMProj/src/test/java/OrangeHRMPkg/BrowserLaunchScript.java
@@ -0,0 +1,17 @@
+package OrangeHRMPkg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserLaunchScript {
+	@Test
+	public void launchBrowser() {
+
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	}
+
+}
